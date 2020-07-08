1dcf748b32fd6feefe17ba1ca905ad55530c6554
viral-sangani <viral.sangani2011@gmail.com>
2020-07-08 11:07:11 +0530

    Added projects in UserDatails page :star2:

--- src/pages/DevDetailsPage/DevDetailsPage.tsx
+++ src/pages/DevDetailsPage/DevDetailsPage.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useData } from "../../contexts/DataContext";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import { AllProject } from "../../utils/contracts";
 
 interface DevDetailsPageProps {}
 const DevDetailsPage: React.FC<DevDetailsPageProps> = (props) => {
   let { username } = useParams();
 
   const { data } = useData();
-  const currUser = data?.allUsers.find((user) => user.login == username);
+  const currUser = data?.allUsers.find((user) => user.login === username);
+
+  var userProject: AllProject[] = [];
+  data?.allProjects.map((project) => {
+    if (currUser?.projects.includes(project.id)) {
+      userProject.push(project);
+    }
+  });
   return (
     <div className="py-8">
       <div className="bg-white rounded-lg p-6 container mx-auto shadow-md">
@@ -21,6 +31,7 @@ const DevDetailsPage: React.FC<DevDetailsPageProps> = (props) => {
             <h3 className="text-xl">{currUser?.name || currUser?.login}</h3>
             <a
               target="_blank"
+              rel="noopener noreferrer"
               href={`https://github.com/${currUser?.login}`}
               className="text-indigo-800"
             >
@@ -33,7 +44,39 @@ const DevDetailsPage: React.FC<DevDetailsPageProps> = (props) => {
       <div className="container mx-auto">
         <h2 className="mt-10 text-lg text-gray-600 mb-2">Projects</h2>
         <div className="card-grid">
-          <div className="bg-white rounded-lg p-6 shadow-md"></div>
+          {userProject &&
+            Object.values(userProject).map((project) => (
+              <Link
+                key={project.id}
+                to={{
+                  pathname: `/project/${project.number}`,
+                  state: { reviewMode: false },
+                }}
+                className="rounded shadow-md px-6 py-6 bg-white w-full flex justify-between flex-col"
+              >
+                <div>
+                  {!!project.title?.length && (
+                    <div className="text-xl font-bold mb-4">
+                      {project.title}
+                    </div>
+                  )}
+                  <ReactMarkdown
+                    className="text-sm text-gray-600 formatted"
+                    source={project.body}
+                  />
+                </div>
+                <div className="flex mt-4 items-center">
+                  <img
+                    src={project.author.avatarUrl}
+                    alt={project.author.name || project.author.login}
+                    className="h-8 w-8 md:h-8 md:w-8 rounded-full mx-auto md:mx-0 md:mr-6"
+                  />
+                  <div className="text-base flex-1">
+                    {project.author.name || project.author.login}
+                  </div>
+                </div>
+              </Link>
+            ))}
         </div>
         <h2 className="mt-10 text-lg text-gray-600 mb-2">Blogs</h2>
         <div className="card-grid">
