bb45d8bbc3676728cda0e323551635b3d187c981
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2020-07-08 11:00:00 +0530

    added sorting to the dev list

--- src/pages/DevListPage/DevListPage.tsx
+++ src/pages/DevListPage/DevListPage.tsx
@@ -8,46 +8,63 @@ const DevListPage: React.FC<DevListPageProps> = (props) => {
   return (
     <div className="container my-12 mx-auto">
       <div className="flex flex-wrap -mx-1 lg:-mx-4">
-        {data?.allUsers.map((user) => (
-          <Link
-            to={`/dev/${user.login}`}
-            className="px-1 w-full md:w-1/2 my-10 lg:px-4 lg:w-1/3"
-          >
-            <div className="relative h-16 bg-white">
-              <img
-                style={{
-                  position: "absolute",
-                  top: "-2.5rem",
-                  left: "50%",
-                  transform: "translate(-50%, 0)",
-                }}
-                className="w-20 h-20 rounded-full"
-                src={user.avatarUrl}
-                alt=""
-              />
-            </div>
-            <article className="overflow-hidden rounded-lg shadow-md bg-white">
-              <div className="px-6 pb-4 bg-white">
-                <div className="text-xl text-center">
-                  {user.name || user.login}
-                </div>
-                <div className="text-xs text-indigo-800 text-gray-700 text-center ">
-                  <a
-                    className="no-underline hover:underline focus:text-gray-900 hover:text-gray-900"
-                    href={user.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    @{user.login}
-                  </a>
-                </div>
-                <div className="text-orange-400 text-xs text-center">
-                  {user.totalProjects} Projects
-                </div>
+        {data?.allUsers
+          .sort((a, b) => {
+            if (a.totalProjects < b.totalProjects) {
+              return 1;
+            }
+            if (a.totalProjects > b.totalProjects) {
+              return -1;
+            }
+            return 0;
+          })
+          .map((user) => (
+            <Link
+              to={`/dev/${user.login}`}
+              className="px-1 w-full md:w-1/2 my-10 lg:px-4 lg:w-1/3"
+            >
+              <div className="relative h-16 bg-white">
+                <img
+                  style={{
+                    position: "absolute",
+                    top: "-2.5rem",
+                    left: "50%",
+                    transform: "translate(-50%, 0)",
+                  }}
+                  className="w-20 h-20 rounded-full"
+                  src={user.avatarUrl}
+                  alt=""
+                />
               </div>
-            </article>
-          </Link>
-        ))}
+              <article className="overflow-hidden rounded-lg shadow-md bg-white">
+                <div className="px-6 pb-4 bg-white">
+                  <div className="text-xl text-center">
+                    {user.name || user.login}
+                  </div>
+                  <div className="text-xs text-indigo-800 text-gray-700 text-center ">
+                    <a
+                      className="no-underline hover:underline focus:text-gray-900 hover:text-gray-900"
+                      href={user.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      @{user.login}
+                    </a>
+                  </div>
+                  <div
+                    className={`${
+                      user.totalProjects >= 5
+                        ? "bg-green-600 text-white"
+                        : "text-orange-400"
+                    } text-xs text-center py-1 w-20 mx-auto rounded-md mt-2`}
+                  >
+                    {user.totalProjects} Project
+                    {user.totalProjects > 1 ? "s" : ""}
+                  </div>
+                </div>
+              </article>
+            </Link>
+          ))}
       </div>
     </div>
   );
